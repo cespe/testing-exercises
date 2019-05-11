28425b118c120bffd98ad840728490f9151c8ef5
Carl Espe <carl@cpespe.com>
2019-05-10 18:06:10 -0700

    Write nine failing tests for findIndex

--- findIndex/findIndex-tests.js
+++ findIndex/findIndex-tests.js
@@ -8,16 +8,19 @@
 tests({
 
 	'findIndex should pass in the ith element as the first argument to the callback': function() {
+		fail();
 		findIndex([1], function(value) {
 			eq(value, 1);
 		});
 	},
 	'findIndex should pass in the ith index as the second argument to the callback': function() {
+		fail();
 		findIndex([1], function(value, index) {
 			eq(index, 0);
 		});
 	},
 	'findIndex should run the callback initial array.length times': function() {
+		fail();
 		var timesCalled = 0;
 		var testArray = [1, 2, 3];
 		var timesToCall = testArray.length;
@@ -31,35 +34,41 @@ tests({
 		eq(testArray.length, 4);
 	},
 	'findIndex should pass in the array as the third argument to the callback': function() {
+		fail();
 		var testArray = [1];
 		findIndex(testArray, function(value, index, array) {
 		eq(array, testArray);
 		});
 	},
 	'findIndex should accept an alternate "this" object to bind to': function() {
+		fail();
 		var altThis = {alternate: true};
 		findIndex([1], function() {
 		eq(altThis, this);
 		}, altThis);
 	},
 	'findIndex should bind "this" to undefined if no alternate "this" is given': function() {
+		fail();
 		findIndex([1], function() {
 		eq(this, undefined);
 		});
 	},
 	'findIndex should return the first element that callback returns "true" for': function() {
+		fail();
 		var result = findIndex([1, 2, 3], function(value) {
 		return value > 1;
 		});
 		eq(result, 2);
 	},
 	'findIndex should return undefined if callback never returns true': function() {
+		fail();
 		var result = findIndex([1, 2, 3], function(value) {
 		return value > 3;
 		});
 		eq(result, undefined);
 	},
 	'findIndex should run callback on missing elements': function() {
+		fail();
 		var timesCalled = 0;
 		var sparseArray = [, 1, 2, , 3];
 		var result = findIndex(sparseArray, function(value) {
