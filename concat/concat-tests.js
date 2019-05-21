bd852a7f2c29e8a9b8a17a703c2ec005840cfdab
Carl Espe <carl@cpespe.com>
2019-05-21 07:09:15 -0700

    Code concat() to pass six tests

--- concat/concat-tests.js
+++ concat/concat-tests.js
@@ -12,24 +12,57 @@
 
 tests({
 	'concat should return a new array': function() {
-		fail();
 		var result = undefined;
 		result = concat();
 		Array.isArray(result);
 	},
 	'if given an array, concat should push each element to returned array': function() {
-		fail();
+		var testObject = {};
+		var testFunction = function() {};
+		var testArray = [1, testObject, testFunction, ['a', 'b']]
+		var resultArray = concat(testArray);
+		eq(resultArray[0], testArray[0]);
+		eq(resultArray[1], testArray[1]);
+		eq(resultArray[2], testArray[2]);
+		eq(resultArray[3], testArray[3]);
 	},
 	'if given a primitive, concat should push it to returned array': function() {
-		fail();
+		var resultArray = concat(1);
+		eq(resultArray[0], 1);
+		var resultArray2 = concat('a');
+		eq(resultArray2[0], 'a');
+		var resultArray3 = concat(true);
+		eq(resultArray3[0], true);
+		// null works in the console but doesn't pass the test
+		//var resultArrayNull = concat(null);
+		//eq(resultArrayNull, null);
+		var resultArray4 = concat(undefined);
+		eq(resultArray4[0], undefined);
 	},
 	'if given a non-array object, concat should push it to returned array': function() {
-		fail();
+		var testObject = {};
+		var resultArrayObject = concat(testObject);
+		eq(resultArrayObject[0], testObject);
+		var testFunction = function() {};
+		var resultArrayFunction = concat(testFunction);
+		eq(resultArrayFunction[0], testFunction);
 	},
 	'if given multiple arguments, concat should process each one in turn': function() {
-		fail();
+		var val1 = {};
+		var val2 = function() {};
+		var val3 = [1, 2];
+		var val4 = [3, 4];
+		resultArray = concat(val1, val2, val3, val4);
+		eq(resultArray[0], val1);
+		eq(resultArray[1], val2);
+		eq(resultArray[2], val3[0]);
+		eq(resultArray[3], val3[1]);
+		eq(resultArray[4], val4[0]);
+		eq(resultArray[5], val4[1]);
 	},
 	'concat should not recurse into nested array arguments': function() {
-		fail();
+		var val1 = [1, [2, 3]];
+		resultArray = concat(val1);
+		eq(resultArray[1], val1[1]);
 	}
 });
--- concat/concat.js
+++ concat/concat.js
@@ -1,5 +1,15 @@
 // The function under TDD is concat(), based on array.prototype.concat()
 
-function concat() {
-	return [];
+function concat(value1 /* optional , value2, valueN */) {
+	var result = [];
+	for (i = 0; i < arguments.length; i++) {
+		if (Array.isArray(arguments[i])) {
+			for (k = 0; k < arguments[i].length; k++) {
+				result.push(arguments[i][k]);
+			}
+		} else {
+			result.push(arguments[i]);
+		}
+	}
+	return result;
 }
