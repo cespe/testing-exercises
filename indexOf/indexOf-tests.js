3d9c8f3721272270ba48de0145c95c9e27a7b345
Carl Espe <carl@cpespe.com>
2019-05-22 07:12:48 -0700

    Write seven failing tests for indexOf

--- indexOf/indexOf-tests.js
+++ indexOf/indexOf-tests.js
@@ -1,21 +1,47 @@
 /*
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 
-Returns the first index at which a given element can be found in the array
-If the element is not in the array, returns -1
+arr.indexOf(searchElement[, fromIndex])
+
+Return the first index at which a given element can be found in the array.
+If the element is not in the array, return -1.
 Either way, returns a number
 Works left to right
 Compares the search element to each array element using strict equality (===)
    which means that the types must match and the contents must match
-By default, the comparison starts at index 0
-The starting index can be specified with an optional fromIndex parameter
-   If fromIndex >= array.length, return -1 and do not search the array
+By default, the comparison starts at index 0.
+The starting index can be specified with an optional fromIndex parameter.
+   If fromIndex >= array.length, return -1 and do not search the array.
    If fromIndex is a negative number, use it as an offset to array.length.
-arr.indexOf(searchElement[, fromIndex])
+      If the offset is > array.length, start at 0.
 */
 
 tests({
-	'': function() {
-
+	'indexOf should return -1 if searchElement is not in the array': function() {
+		fail();
+		result = indexOf(['a'], 'b');
+		eq(result, -1);
+	},
+	'indexOf should start search at array[0] if fromIndex is not provided': function() {
+		fail();
+		result = indexOf(['a', 'a'], 'a');
+		eq(result, 0);
+	},
+	'indexOf should return index of first element matching searchElement': function() {
+		fail();
+		result = indexOf(['a', 'b', 'b'], 'b');
+		eq(result, 1);
+	},
+	'indexOf should start search at array[fromIndex] if it is provided': function() {
+		fail();
+	},
+	'If fromIndex is >= array.length, indexOf should return -1 without searching array': function() {
+		fail();
+	},
+	'If fromIndex < 0 and |fromIndex| > array.length, starting index should be 0': function() {
+		fail();
+	},
+	'If fromIndex < 0 and |fromIndex| < array.length, starting index should be array.length - |fromIndex|': function() {
+		fail();
 	}
 });
--- indexOf/indexOf.js
+++ indexOf/indexOf.js
@@ -1,3 +1,11 @@
 // The function under test is indexOf(), based on array.prototype.indexOf()
 
-
+function indexOf(array, searchElement) {
+	var result = -1;
+	for (i = 0; i < array.length; i++) {
+		if (array[i] === searchElement) {
+			return i;
+		}
+	return result
+	}
+}
