b3877eef0120d1fef69a4be0cc8e50b6aee3e2d5
Carl Espe <carl@cpespe.com>
2019-05-30 15:24:05 -0700

    Document behavior for objects resembling arrays

--- unshift/unshift-tests.js
+++ unshift/unshift-tests.js
@@ -4,8 +4,36 @@ unshift(array) adds one or more elements to the beginning of array.
 
 Returns new length of array.
 
-Same general behavior as push().
+Same general behavior as push() for objects resembling arrays:
+	If no length property, unshift creates it
+	If length property is not a number, unshift sets it to 0
+	If length property is not an integer, unshift converts it
 
 */
 
-
+tests({
+	'unshift should take an array and return its length': function() {
+		fail();
+	},
+	'unshift should take zero or more values and prepend them to array': function() {
+		fail();
+	},
+	'': function() {
+		fail();
+	},
+	'': function() {
+		fail();
+	},
+	'': function() {
+		fail();
+	},
+	'': function() {
+		fail();
+	},
+	'': function() {
+		fail();
+	},
+	'': function() {
+		fail();
+	},
+});
