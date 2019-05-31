5a504aa13e2f24af6c0c9bfecdb0f5a3fd128320
Carl Espe <carl@cpespe.com>
2019-05-31 16:01:35 -0700

    Add two fill() tests for parameters

--- fill/fill-tests.js
+++ fill/fill-tests.js
@@ -3,7 +3,9 @@
 The fill() method fills (modifies) all the elements of an array from a start index
 (default zero) to an end index (default array length) with a static value.
 
-It returns the modified array.
+Like splice, end is the number after the final index.
+
+fill() returns the modified array.
 
 arr.fill(value[, start[, end]])
 
@@ -14,22 +16,47 @@ fill works on generic objects too, but only if they have a length convertible to
 	If no length property, don't modify and don't add add one
 	If length property is convertible e.g. '2', fill as normal but don't change length from '2'
 	If length property is not convertible, don't modify and don't fill
-
 */
 
 tests({
 	'fill should take an array and return it': function() {
-		fail();
+		target = [1];
+		result = fill(target);
+		eqstrict(result, target);
 	},
 	'fill should take a value and set array elements to it': function() {
-		fail();
+		target = [1];
+		result = fill(target, 'x');
+		eqstrict(result, target);
+		eqstrict(target[0], 'x');
 	},
-	'if a start value is not provided, fill should default to 0': function() {
-		fail();
+	'if a start value is not provided, start should default to 0': function() {
+		target = [1, 2];
+		result = fill(target, 'x');
+		eqstrict(result, target);
+		eqstrict(target[0], 'x');
 	},
-	'if an end value is not provided, fill should default to array.length': function() {
-		// like splice, end is the number after the final index
-		fail();
+	'if an end value is not provided, end should default to array.length': function() {
+		target = [1, 2];
+		result = fill(target, 'x');
+		eqstrict(result, target);
+		eqstrict(target[1], 'x');
+	},
+	'If start is provided, fill should use it as starting index': function() {
+		target = [1, 2, 3];
+		result = fill(target, 'x', 1);
+		eqstrict(result, target);
+		eqstrict(target[0], 1);
+		eqstrict(target[1], 'x');
+		eqstrict(target[2], 'x');
+	},
+	'If end is provided, fill should use it as final index + 1': function() {
+		target = [1, 2, 3];
+		result = fill(target, 'x', 1, 2);
+		eqstrict(result, target);
+		eqstrict(target[0], 1);
+		eqstrict(target[1], 'x');
+		eqstrict(target[2], 3);
 	},
 	'If start is negative, fill should start at index array.length + start': function() {
 		fail();
--- fill/fill.js
+++ fill/fill.js
@@ -1,3 +1,17 @@
 // function fill() based on array.prototype.fill()
 
-
+function fill(array, value, start, end) {
+	if (arguments.length < 2) {
+		return array;
+	} else
+		if (arguments.length < 3) {
+			var start = 0;
+		}
+		if (arguments.length < 4) {
+			var end = array.length;
+		}
+		for (var i = start; i < end; i++) {
+			array[i] = value;
+		}
+		return array;
+}
