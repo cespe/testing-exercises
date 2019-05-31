900917b92faaa2ae6ca0995e4222e6bbf331e859
Carl Espe <carl@cpespe.com>
2019-05-31 14:56:23 -0700

    Set up TDD files for fill() and take spec notes

---
+++ fill/fill-tests.js
@@ -0,0 +1,17 @@
+/*
+ 
+The fill() method fills (modifies) all the elements of an array from a start index
+(default zero) to an end index (default array length) with a static value.
+
+It returns the modified array.
+
+arr.fill(value[, start[, end]])
+
+If start is negative, start at array.length + start
+If end is negative, end at array.length + end
+
+fill should work on generic objects too.
+
+*/
+
+
---
+++ fill/fill.js
@@ -0,0 +1,3 @@
+// function fill() based on array.prototype.fill()
+
+
