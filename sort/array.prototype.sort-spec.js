a7daf1648a5b3bb866fd6e872fbba65f3b68c0a4
Carl Espe <carl@cpespe.com>
2019-06-07 18:44:58 -0700

    Write three more tests for sort(), code function to pass first eight

--- sort/array.prototype.sort-spec.js
+++ sort/array.prototype.sort-spec.js
@@ -31,15 +31,43 @@ function compare(a, b) {
   // a must be equal to b
   return 0;
 }
-
-a - b serves to sort numbers.
-
-Sort algorithm
-Want to move elements left until they are in the proper place
-While not sorted
-For each element in the array
-	compare to following element
-		swap if appropriate
-
 */
+
+[[1, 30, 100000], 'd', 'a'].sort()
+//(3) [Array(3), "a", "d"]
+
+['d', [100000, 1], 'a'].sort()
+//(3) [Array(2), "a", "d"]
+
+[1, 30, 4, 21, 100000].sort()
+//(5) [1, 100000, 21, 30, 4]
+
+[true, false, true, false].sort()
+//(4) [false, false, true, true]
+
+[null, NaN, undefined].sort()
+//(3) [NaN, null, undefined]
+
+[{}, [1], function(){}, 'a', true, NaN, null, undefined, 88].sort()
+//(9) [Array(1), 88, NaN, {…}, "a", ƒ, null, true, undefined]
+
+[undefined,,88,,14, undefined].sort()
+//(6) [14, 88, undefined, undefined, empty × 2]
+
+[88, 1000, NaN, Infinity, 14].sort()
+//(5) [1000, 14, 88, Infinity, NaN]
+
+[88, 1000, NaN, Infinity, 14].sort(function(a, b) {
+	return a - b;
+});
+(5) [88, 1000, NaN, 14, Infinity]
+
+var numbers = [4, 2, 10, 5, 1, 3];
+function compareNumbers(a, b) {
+	return a - b;
+}
+numbers.sort(compareNumbers)
+//(6) [1, 2, 3, 4, 5, 10]
+
+
 
--- sort/sort-tests.js
+++ sort/sort-tests.js
@@ -1,26 +1,74 @@
 // based on array.prototype.sort(). See spec notes in array.prototype.sort-spec.js
 
 tests({
-	'sort should take an array and return it sorted': function() {
-		fail();
+	'sort should take an array and return it': function() {
+		var arr = ['a'];
+		var result = sort(arr);
+		eqstrict(result, arr);
 	},
 	'If no callback is provided, sort should sort elements into UTF-16 order': function() {
-		// function as coded leaves this to Javascript string comparison
+		// function as coded leaves this to Javascript string comparison; could use a test case
+		var arr1 = ['b', 'd', 'c', 'a'];
+		var result1 = sort(arr1);
+		eqstrict(arr1[0], 'a');
+		eqstrict(arr1[1], 'b');
+		eqstrict(arr1[2], 'c');
+		eqstrict(arr1[3], 'd');
+		var months = ['March', 'Jan', 'Feb', 'Dec'];
+		var monthsResult = sort(months);
+		eq(months[0], 'Dec');
+		eq(months[1], 'Feb');
+		eq(months[2], 'Jan');
+		eq(months[3], 'March');
+	},
+	'If no callback provided, sort should convert non-string elements to strings for sorting': function() {
+		var arr2 = [1, 30, 4, 21, 100000];		// test numbers converted to strings
+		var result2 = sort(arr2);
+		eqstrict(arr2[0], 1);
+		eqstrict(arr2[1], 100000);
+		eqstrict(arr2[2], 21);
+		eqstrict(arr2[3], 30);
+		eqstrict(arr2[4], 4);
+	},
+	'sort should pass a first element for comparison as the first argument to the callback': function() {
+		sort(['a', 'b'], function(a) {
+			eq(a, 'a');
+		});
+	},
+	'sort should pass a second, later element for comparison as the second argument to the callback': function() {
+		sort(['a', 'b'], function(a, b) {
+			eq(b, 'b');
+		});
+	},
+	'Callback should return a negative number if first element < second element'() {
+		// no code change, only the callback is tested
+		function compareTest(a, b) {
+			return a - b;
+		}
+		result = compareTest(1, 3);
+		eq(result, -2);
+	},
+	'Callback should return a positive number if first element > second element'() {
+		// no code change, only the callback is tested
+		function compareTest(a, b) {
+			return a - b;
+		}
+		result = compareTest(3, 1);
+		eq(result, 2);
+	},
+	'Callback should return 0 if first element == second element'() {
+		// no code change, only the callback is tested
+		function compareTest(a, b) {
+			return a - b;
+		}
+		result = compareTest(3, 3);
+		eq(result, 0);
+	},
+	'sort should put second element before the first if callback returns a positive number': function() {
+	// true but vague; the implementation swaps the two elements
 		fail();
 	},
-	"If provided, callback should take two elements firstEl and secondEl": function() {
-		fail();
-	},
-	'Callback should return a negative number if firstEl < secondEl'() {
-		fail();
-	},
-	'Callback should return a positive number if firstEl > secondEl'() {
-		fail();
-	},
-	'Callback should return 0 if firstEl == secondEl'() {
-		fail();
-	},
-	'If callback is provided, sort should sort based on callback return values': function() {
+	'sort should move all empty index positions to the end of the array': function() {
 		fail();
 	}
 });
