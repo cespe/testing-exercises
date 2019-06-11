d832dcac64f0560bc60b69465f3a2dd667b28c8e
Carl Espe <carl@cpespe.com>
2019-06-11 13:43:52 -0700

    Code splice() to pass first seven tests covering required parameters

--- splice/splice-tests.js
+++ splice/splice-tests.js
@@ -28,25 +28,56 @@
 
 tests({
 	'splice should accept an array as its first argument and return a new array': function() {
-		fail();
+		var arr = [1, 2, 3];
+		result = splice(arr);
+		eqstrict(result === arr, false);
+		eq(Array.isArray(result), true);
 	},
-	"splice's second argument should be start, the index at which to begin splicing": function() {
-		fail();
+	'splice should take start, a starting index number, as its second argument': function() {
+		// added param to the function, but the code change isn't needed to pass the test
+		var arr1 = [1, 2, 3];
+		result1 = splice(arr1, 0);
+		eq(Array.isArray(result1), true);
 	},
-	'Without optional arguments, splice should delete elements from start index to end of array': function() {
-		fail();
+	'If only array and start are provided, splice should remove elements from start index to end of array': function() {
+		var arr2 = [1, 2, 3];
+		result2 = splice(arr2, 1);
+		eqstrict(arr2.length, 1);
+		eqstrict(arr2[0], 1);
+		eqstrict(arr2[1] in arr2, false);
+		eqstrict(arr2[2] in arr2, false);
 	},
-	'splice should put deleted elements, if any, in the returned array': function() {
-		fail();
+	'splice should put deleted elements, if any, in the array to be returned': function() {
+		var arr3 = [1, 2, 3];
+		result3 = splice(arr3, 1);
+		eqstrict(result3.length, 2);
+		eqstrict(result3[0], 2);
+		eqstrict(result3[1], 3);
 	},
 	'If start > array.length, splice should set start to array.length': function() {
-		fail();
+		var arr4 = [1, 2, 3];
+		result4 = splice(arr4, 5);
+		eqstrict(arr4.length, 3);
+		eqstrict(arr4[0], 1);
+		eqstrict(arr4[1], 2);
+		eqstrict(arr4[2], 3);
+	
 	},
 	'If start < 0, splice should set start to array.length + start': function() {
-		fail();
+		var arr5 = [1, 2, 3];
+		result5 = splice(arr5, -2);
+		eqstrict(arr5.length, 1);
+		eqstrict(arr5[0], 1);
+		eqstrict(arr5[1] in arr5, false);
+		eqstrict(arr5[2] in arr5, false);
 	},
 	'If start < 0 and array.length + start < 0, splice should set start 0': function() {
-		fail();
+		var arr6 = [1, 2, 3];
+		result6 = splice(arr6, -5);
+		eqstrict(arr6.length, 0);
+		eqstrict(arr6[0] in arr6, false);
+		eqstrict(arr6[1] in arr6, false);
+		eqstrict(arr6[2] in arr6, false);
 	},
 	'splice should accept a third argument, deleteCount, and remove that number of elements': function() {
 		fail();
--- splice/splice.js
+++ splice/splice.js
@@ -1,1 +1,27 @@
 // function splice() based on array.prototype.splice()
+
+function splice(array, start) {
+	var result = [];
+
+	if (start > array.length) {
+		start = array.length;
+	}
+	if (start < 0) {
+		start = array.length + start;
+		if (start < 0) {
+			start = 0;
+		}
+	}
+	if (arguments.length < 2) {
+		return []
+	}
+	if (arguments.length === 2) {
+		var k = 0;
+		for (var i = start; i < array.length; i++) {
+			result[k] = array[i];	
+			k++;
+		}
+		array.length = array.length - k;	// after loop, k is the number of removed elements
+	}
+	return result;
+}
