3213b6c75418dc75fe3ed113c71cda24473f5598
Carl Espe <carl@cpespe.com>
2019-06-14 13:32:38 -0700

    Replace eqstrict with eq in splice tests

--- splice/splice-tests.js
+++ splice/splice-tests.js
@@ -1,211 +1,213 @@
 // tests for splice(), based on array.prototype.splice()
 // covers standard behavior
 // covers behavior with parameters of the wrong type
-// does not cover objects resembling arrays 
+// does not cover objects resembling arrays, at least initially
+//
+// eq is an alias for assertStrictEquals in the version of simpletest used here.
 
 tests({
 	'splice should accept an array as its first argument and return a new array': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr);
-		eqstrict(result === arr, false);
+		eq(result === arr, false);
 		eq(Array.isArray(result), true);
 	},
 	'splice should accept a number, start, as its second argument, and if only array and start are given, it should remove elements from start to end of array': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, 1);
-		eqstrict(arr.length, 1);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1] in arr, false);
-		eqstrict(arr[2] in arr, false);
+		eq(arr.length, 1);
+		eq(arr[0], 1);
+		eq(arr[1] in arr, false);
+		eq(arr[2] in arr, false);
 	},
 	'splice should put deleted elements, if any, in the array to be returned': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, 1);
-		eqstrict(result.length, 2);
-		eqstrict(result[0], 2);
-		eqstrict(result[1], 3);
+		eq(result.length, 2);
+		eq(result[0], 2);
+		eq(result[1], 3);
 
 		var arr = [];
 		result = splice(arr, 1);
-		eqstrict(result.length, 0)
-		eqstrict(arr.length, 0);
+		eq(result.length, 0)
+		eq(arr.length, 0);
 	},
 	'If start > array.length, splice should set start to array.length': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, 5);
-		eqstrict(arr.length, 3);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
-		eqstrict(arr[2], 3);
+		eq(arr.length, 3);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
+		eq(arr[2], 3);
 	},
 	'If start < 0, splice should set start to array.length + start': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, -2);
-		eqstrict(arr.length, 1);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1] in arr, false);
-		eqstrict(arr[2] in arr, false);
+		eq(arr.length, 1);
+		eq(arr[0], 1);
+		eq(arr[1] in arr, false);
+		eq(arr[2] in arr, false);
 	},
 	'If start < 0 and array.length + start < 0, splice should set start to 0': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, -5);
-		eqstrict(arr.length, 0);
-		eqstrict(arr[0] in arr, false);
-		eqstrict(arr[1] in arr, false);
-		eqstrict(arr[2] in arr, false);
+		eq(arr.length, 0);
+		eq(arr[0] in arr, false);
+		eq(arr[1] in arr, false);
+		eq(arr[2] in arr, false);
 	},
 	'If start is non-numeric, splice should convert it to an integer if possible': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, '1.2');
-		eqstrict(result.length, 2);
-		eqstrict(result[0], 2);
-		eqstrict(result[1], 3);
-		eqstrict(arr.length, 1);
-		eqstrict(arr[0], 1);
+		eq(result.length, 2);
+		eq(result[0], 2);
+		eq(result[1], 3);
+		eq(arr.length, 1);
+		eq(arr[0], 1);
 	},
 	'If start is undefined or evaluates to NaN, splice should set it to 0': function() {
 		var arr = [1, 2];
 		result = splice(arr, 'xyz');
-		eqstrict(result.length, 2);
-		eqstrict(result[0], 1);
-		eqstrict(result[1], 2);
-		eqstrict(arr.length, 0);
+		eq(result.length, 2);
+		eq(result[0], 1);
+		eq(result[1], 2);
+		eq(arr.length, 0);
 
 		var arr = [1, 2];
 		result = splice(arr, undefined);
-		eqstrict(result.length, 2);
-		eqstrict(result[0], 1);
-		eqstrict(result[1], 2);
-		eqstrict(arr.length, 0);
+		eq(result.length, 2);
+		eq(result[0], 1);
+		eq(result[1], 2);
+		eq(arr.length, 0);
 	},
 	'splice should accept a number, deleteCount, as its third argument and remove that number of elements if given': function() {
 		var arr = [1, 2, 3, 4, 5];
 		result = splice(arr, 1, 3);		// deleteCount > start
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 5);
+		eq(arr[0], 1);
+		eq(arr[1], 5);
 		eq(arr.length, 2);
-		eqstrict(result[0], 2);
-		eqstrict(result[1], 3);
-		eqstrict(result[2], 4);
-		eqstrict(result.length, 3);
+		eq(result[0], 2);
+		eq(result[1], 3);
+		eq(result[2], 4);
+		eq(result.length, 3);
 
 		var arr = [1, 2, 3, 4, 5];
 		result = splice(arr, 3, 1);		// start > deleteCount
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
-		eqstrict(arr[2], 3);
-		eqstrict(arr[3], 5);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
+		eq(arr[2], 3);
+		eq(arr[3], 5);
 		eq(arr.length, 4);
-		eqstrict(result[0], 4);
-		eqstrict(result.length, 1);
+		eq(result[0], 4);
+		eq(result.length, 1);
 
 		var arr = [1, 2, 3, 4, 5];
 		result = splice(arr, 0, 5);		// delete all
-		eqstrict(result.length, 5);
-		eqstrict(result[0], 1);
-		eqstrict(result[1], 2);
-		eqstrict(result[2], 3);
-		eqstrict(result[3], 4);
-		eqstrict(result[4], 5);
+		eq(result.length, 5);
+		eq(result[0], 1);
+		eq(result[1], 2);
+		eq(result[2], 3);
+		eq(result[3], 4);
+		eq(result[4], 5);
 		eq(arr.length, 0);
 
 		var arr = [1, 2, 3];
 		result = splice(arr, 0, 0);		// delete none
-		eqstrict(result.length, 0);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
-		eqstrict(arr[2], 3);
-		eqstrict(arr.length, 3);
+		eq(result.length, 0);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
+		eq(arr[2], 3);
+		eq(arr.length, 3);
 
 		var arr = [];
 		result = splice(arr, 1, 1);		// delete not possible
-		eqstrict(result.length, 0)
-		eqstrict(arr.length, 0);
+		eq(result.length, 0)
+		eq(arr.length, 0);
 	},
 	'If deleteCount >= (array.length - start), splice should remove elements to end of array': function() {
 		var arr = [1, 2, 3, 4, 5];
 		result = splice(arr, 1, 7);
-		eqstrict(arr[0], 1);
+		eq(arr[0], 1);
 		eq(arr.length, 1);
-		eqstrict(result[0], 2);
-		eqstrict(result[1], 3);
-		eqstrict(result[2], 4);
-		eqstrict(result[3], 5);
-		eqstrict(result.length, 4);
+		eq(result[0], 2);
+		eq(result[1], 3);
+		eq(result[2], 4);
+		eq(result[3], 5);
+		eq(result.length, 4);
 	},
 	'If deleteCount <= 0, splice should not remove any elements': function() {
 		var arr = [1, 2];
 		result = splice(arr, 1, 0);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
 		eq(arr.length, 2);
-		eqstrict(result.length, 0);
+		eq(result.length, 0);
 
 		var arr = [1, 2];
 		result = splice(arr, 1, -1);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
 		eq(arr.length, 2);
-		eqstrict(result.length, 0);
+		eq(result.length, 0);
 	},
 	'If deleteCount is non-numeric, splice should convert it to an integer if possible': function() {
 		var arr = [1, 2, 3];
 		result = splice(arr, 0, '1.2');
-		eqstrict(result.length, 1);
-		eqstrict(result[0], 1);
-		eqstrict(arr.length, 2);
-		eqstrict(arr[0], 2);
-		eqstrict(arr[1], 3);
+		eq(result.length, 1);
+		eq(result[0], 1);
+		eq(arr.length, 2);
+		eq(arr[0], 2);
+		eq(arr[1], 3);
 	},
 	'If deleteCount is undefined or evaluates to NaN, splice should set it to 0': function() {
 		var arr = [1, 2];
 		result = splice(arr, 0, 'xyz');
-		eqstrict(result.length, 0);
-		eqstrict(arr.length, 2);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
+		eq(result.length, 0);
+		eq(arr.length, 2);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
 
 		var arr = [1, 2];
 		result = splice(arr, 0, undefined);
-		eqstrict(result.length, 0);
-		eqstrict(arr.length, 2);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
+		eq(result.length, 0);
+		eq(arr.length, 2);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
 	},
 	'If additional arguments are supplied, splice should add them as elements beginning from start': function() {
 		var arr = [1, 3];
 		result = splice(arr, 1, 0, 2);
-		eqstrict(arr[0], 1);
-		eqstrict(arr[1], 2);
-		eqstrict(arr[2], 3);
-		eqstrict(result.length, 0);
-		eqstrict(arr.length, 3);
+		eq(arr[0], 1);
+		eq(arr[1], 2);
+		eq(arr[2], 3);
+		eq(result.length, 0);
+		eq(arr.length, 3);
 
 		var arr = ['a', 'b', 'f', 'f', 'g'];
 		result = splice(arr, 2, 1, 'c', 'd', 'e');
-		eqstrict(result.length, 1);
-		eqstrict(result[0], 'f');
-		eqstrict(arr.length, 7);
-		eqstrict(arr[0], 'a');
-		eqstrict(arr[1], 'b');
-		eqstrict(arr[2], 'c');
-		eqstrict(arr[3], 'd');
-		eqstrict(arr[4], 'e');
-		eqstrict(arr[5], 'f');
-		eqstrict(arr[6], 'g');
+		eq(result.length, 1);
+		eq(result[0], 'f');
+		eq(arr.length, 7);
+		eq(arr[0], 'a');
+		eq(arr[1], 'b');
+		eq(arr[2], 'c');
+		eq(arr[3], 'd');
+		eq(arr[4], 'e');
+		eq(arr[5], 'f');
+		eq(arr[6], 'g');
 
 		var arr = [1, 2, 3, 4, 5];
 		result = splice(arr, 0, 5, 'a', 'b', 'c', 'd');
-		eqstrict(result.length, 5);
-		eqstrict(result[0], 1);
-		eqstrict(result[1], 2);
-		eqstrict(result[2], 3);
-		eqstrict(result[3], 4);
-		eqstrict(result[4], 5);
-		eqstrict(arr.length, 4);
-		eqstrict(arr[0], 'a');
-		eqstrict(arr[1], 'b');
-		eqstrict(arr[2], 'c');
-		eqstrict(arr[3], 'd');
+		eq(result.length, 5);
+		eq(result[0], 1);
+		eq(result[1], 2);
+		eq(result[2], 3);
+		eq(result[3], 4);
+		eq(result[4], 5);
+		eq(arr.length, 4);
+		eq(arr[0], 'a');
+		eq(arr[1], 'b');
+		eq(arr[2], 'c');
+		eq(arr[3], 'd');
 	}
 });
